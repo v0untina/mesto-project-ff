b066ba522141d08fe4c8fb6b14c95710f92c3ff1
Сашенька <138240993+v0untina@users.noreply.github.com>
2025-03-16 13:23:07 +0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,27 @@
 const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
 
-function createCard(){
-    initialCards.forEach(element => {
-        const card = cardTemplate.cloneNode(true).querySelector(".card");
-        const cardTitle = card.querySelector(".card__title");
-        const cardImage = card.querySelector(".card__image");
-        const deleteButton = card.querySelector(".card__delete-button");
-        cardTitle.textContent = element.name;
-        cardImage.src = element.link;
-        deleteButton.addEventListener("click", ()=>{
-            card.remove()
-        })
-        placesList.append(card)
-    });
+function createCard(cardData, deleteCardHandler) {
+    const card = cardTemplate.cloneNode(true).querySelector(".card");
+    const cardTitle = card.querySelector(".card__title");
+    const cardImage = card.querySelector(".card__image");
+    const deleteButton = card.querySelector(".card__delete-button");
+
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    deleteButton.addEventListener("click", deleteCardHandler);
+
+    return card;
 }
 
+function deleteCard(event) {
+    const card = event.target.closest(".card");
+    card.remove();
+}
 
-createCard()
+initialCards.forEach(cardData => {
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.append(cardElement);
+});
