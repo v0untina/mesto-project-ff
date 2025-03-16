1a4de0fcba7d4f3c2dd25ece6296dd25746c771e
Сашенька <138240993+v0untina@users.noreply.github.com>
2025-03-16 12:25:35 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(){
+    initialCards.forEach(element => {
+        const card = cardTemplate.cloneNode(true).querySelector(".card");
+        const cardTitle = card.querySelector(".card__title");
+        const cardImage = card.querySelector(".card__image");
+        const deleteButton = card.querySelector(".card__delete-button");
+        cardTitle.textContent = element.name;
+        cardImage.src = element.link;
+        deleteButton.addEventListener("click", ()=>{
+            card.remove()
+        })
+        placesList.append(card)
+    });
+}
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+createCard()
